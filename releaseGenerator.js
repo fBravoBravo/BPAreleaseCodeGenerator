8ef060b4204a17a38b4ff676071865c648f822f4
Francisco Bravo <bravofran3@gmail.com>
2024-06-05 15:15:45 +0200

    added hours, minutes and seconds to the string

--- releaseGenerator.js
+++ releaseGenerator.js
@@ -3,7 +3,8 @@
 const commitCode = process.argv[2];
 
 const formattedDate = getFormattedDate();
-console.log(`BPA ${formattedDate}-UTC-${process.argv[2]}`);
+const formattedTime = getFormattedTime();
+console.log(`BPA ${formattedDate}-${formattedTime}-UTC-${process.argv[2]}`);
 
 function getFormattedDate() {
   const today = new Date();
@@ -12,4 +13,14 @@ function getFormattedDate() {
   let day = String(today.getDate()).padStart(2, "0"); // Add leading zero for single-digit days
 
   return `${year}.${month}.${day}`;
+}
+
+
+function getFormattedTime() {
+  const today = new Date();
+  const hours = today.getHours();
+  const minutes = today.getMinutes();
+  const seconds = today.getSeconds();
+
+  return `${hours}.${minutes}.${seconds}`;
 }
